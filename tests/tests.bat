d310c98fc65204cfbefa923f20ab43dcfd4476f9
afanofosc_99 <afanofosc_99@2dfa107a-97e7-4436-bfd7-0c1a33b3a24a>
2009-02-16 22:06:12 +0000

    Updated tests.bat with new tests
    git-svn-id: https://svn.code.sf.net/p/bricxcc/code@37 2dfa107a-97e7-4436-bfd7-0c1a33b3a24a

--- tests/tests.bat
+++ tests/tests.bat
@@ -23,6 +23,8 @@
 @..\nxt\nbc -EF ar_test3.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF block_f.2.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF block_f.2.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF block_f.6.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF block_f.6.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF boolean_test.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF boolean_test.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF bouncy-nocompile.nxc >> output.txt 2>&1
@@ -31,14 +33,28 @@
 @..\nxt\nbc -EF bouncy-workaround.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF e_test.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF e_test.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF -v=128 float.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF -v=128 float.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF f_array_search.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF f_array_search.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF math_array.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF math_array.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF negnum_test.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF negnum_test.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF old_syntax.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF old_syntax.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF param_test.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF param_test.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF rect_f.1.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF rect_f.1.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF rect_f.2.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF rect_f.2.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF rotmot_test.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF rotmot_test.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF strtest.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF strtest.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF strtest2.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF strtest2.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF struct_test1.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF struct_test1.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF struct_test2.nxc >> output.txt 2>&1
@@ -47,3 +63,5 @@
 @..\nxt\nbc -EF struct_tests.nxc >> output.txt 2>&1
 @echo ..\nxt\nbc -EF str_test2.nxc >> output.txt 2>&1
 @..\nxt\nbc -EF str_test2.nxc >> output.txt 2>&1
+@echo ..\nxt\nbc -EF testswitch.nxc >> output.txt 2>&1
+@..\nxt\nbc -EF testswitch.nxc >> output.txt 2>&1
