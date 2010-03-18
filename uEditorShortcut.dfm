6fda0fe38a04ba4feb32274a532b26665c68029e
afanofosc_99 <afanofosc_99@2dfa107a-97e7-4436-bfd7-0c1a33b3a24a>
2010-03-18 17:43:03 +0000

    New unit for editing the editor expert shortcuts.
    git-svn-id: https://svn.code.sf.net/p/bricxcc/code@316 2dfa107a-97e7-4436-bfd7-0c1a33b3a24a

---
+++ uEditorShortcut.dfm
@@ -0,0 +1,62 @@
+object fmEditorShortcut: TfmEditorShortcut
+  Left = 304
+  Top = 219
+  BorderStyle = bsDialog
+  Caption = 'Define Shortcut'
+  ClientHeight = 129
+  ClientWidth = 228
+  Color = clBtnFace
+  Font.Charset = ANSI_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = True
+  Position = poScreenCenter
+  Scaled = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 14
+  object gbxShortcut: TGroupBox
+    Left = 9
+    Top = 8
+    Width = 211
+    Height = 81
+    Caption = 'Editor Expert Name'
+    TabOrder = 0
+    object lblShortCut: TLabel
+      Left = 9
+      Top = 24
+      Width = 48
+      Height = 14
+      Caption = '&Shortcut'
+    end
+    object hkShortCut2: TEdit
+      Left = 9
+      Top = 47
+      Width = 189
+      Height = 22
+      TabOrder = 0
+    end
+  end
+  object btnCancel: TButton
+    Left = 145
+    Top = 96
+    Width = 75
+    Height = 25
+    Cancel = True
+    Caption = 'Cancel'
+    ModalResult = 2
+    TabOrder = 2
+  end
+  object btnOK: TButton
+    Left = 60
+    Top = 96
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    Default = True
+    ModalResult = 1
+    TabOrder = 1
+  end
+end
---
+++ uEditorShortcut.pas
@@ -0,0 +1,71 @@
+(*
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Portions of this code are covered under the GExperts license
+ * http://www.gexperts.org/license.html
+ *
+ * Portions created by John Hansen are Copyright (C) 2009 John Hansen.
+ * All Rights Reserved.
+ *
+ *)
+unit uEditorShortcut;
+
+interface
+
+uses  Classes, Controls, StdCtrls, ComCtrls, Forms, uNewHotKey;
+
+type
+  TfmEditorShortcut = class(TForm)
+    gbxShortCut: TGroupBox;
+    lblShortCut: TLabel;
+    btnCancel: TButton;
+    btnOK: TButton;
+    hkShortCut2: TEdit;
+    procedure FormCreate(Sender: TObject);
+  private
+    hkShortCut: TBricxCCHotKey;
+    procedure CreateHotKeyEdits;
+    function GetShortCut: TShortCut;
+    procedure SetShortCut(const Value: TShortCut);
+  public
+    property ShortCut : TShortCut read GetShortCut write SetShortCut;
+  end;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  uGuiUtils;
+
+procedure TfmEditorShortcut.CreateHotKeyEdits;
+begin
+  hkShortCut  := TBricxCCHotKey.Create(Self);
+  CloneHotKey(hkShortCut, hkShortCut2);
+end;
+
+procedure TfmEditorShortcut.FormCreate(Sender: TObject);
+begin
+  CreateHotKeyEdits;
+end;
+
+function TfmEditorShortcut.GetShortCut: TShortCut;
+begin
+  Result := hkShortCut.HotKey;
+end;
+
+procedure TfmEditorShortcut.SetShortCut(const Value: TShortCut);
+begin
+  hkShortCut.HotKey := Value;
+end;
+
+end.
+
