90cedbe3a210a882fed84bec0fd1e0e01d99478f
afanofosc_99 <afanofosc_99@2dfa107a-97e7-4436-bfd7-0c1a33b3a24a>
2011-06-30 00:37:51 +0000

    Joystick tool enhancements
    git-svn-id: https://svn.code.sf.net/p/bricxcc/code@510 2dfa107a-97e7-4436-bfd7-0c1a33b3a24a

---
+++ uJoyActions.dfm
@@ -0,0 +1,107 @@
+object frmJoyActions: TfrmJoyActions
+  Left = 253
+  Top = 139
+  BorderStyle = bsDialog
+  Caption = 'Joystick Options'
+  ClientHeight = 168
+  ClientWidth = 345
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poOwnerFormCenter
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object grpChangeActions: TGroupBox
+    Left = 8
+    Top = 8
+    Width = 249
+    Height = 153
+    Caption = 'Joystick change action'
+    TabOrder = 0
+    object lblMailbox: TLabel
+      Left = 48
+      Top = 96
+      Width = 101
+      Height = 13
+      Caption = 'NXT &mailbox number:'
+      FocusControl = cboMailbox
+    end
+    object radCADefault: TRadioButton
+      Left = 8
+      Top = 24
+      Width = 231
+      Height = 17
+      Caption = '&Default'
+      Checked = True
+      TabOrder = 0
+      TabStop = True
+      OnClick = ActionsClick
+    end
+    object radCAScript: TRadioButton
+      Left = 8
+      Top = 48
+      Width = 231
+      Height = 17
+      Caption = '&Execute Pascal script'
+      TabOrder = 1
+      OnClick = ActionsClick
+    end
+    object radCAMessage: TRadioButton
+      Left = 8
+      Top = 72
+      Width = 231
+      Height = 17
+      Caption = '&Send message to brick'
+      TabOrder = 2
+      OnClick = ActionsClick
+    end
+    object cboMailbox: TComboBox
+      Left = 48
+      Top = 115
+      Width = 81
+      Height = 21
+      HelpContext = 18016
+      Style = csDropDownList
+      ItemHeight = 13
+      ItemIndex = 0
+      TabOrder = 3
+      Text = 'Mailbox 1'
+      Items.Strings = (
+        'Mailbox 1'
+        'Mailbox 2'
+        'Mailbox 3'
+        'Mailbox 4'
+        'Mailbox 5'
+        'Mailbox 6'
+        'Mailbox 7'
+        'Mailbox 8'
+        'Mailbox 9'
+        'Mailbox 10')
+    end
+  end
+  object btnOK: TButton
+    Left = 264
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    Default = True
+    ModalResult = 1
+    TabOrder = 1
+  end
+  object btnCancel: TButton
+    Left = 264
+    Top = 48
+    Width = 75
+    Height = 25
+    Cancel = True
+    Caption = 'Cancel'
+    ModalResult = 2
+    TabOrder = 2
+  end
+end
---
+++ uJoyActions.pas
@@ -0,0 +1,112 @@
+(*
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Initial Developer of this code is John Hansen.
+ * Portions created by John Hansen are Copyright (C) 2009 John Hansen.
+ * All Rights Reserved.
+ *
+ *)
+unit uJoyActions;
+
+{$IFDEF FPC}
+{$MODE Delphi}
+{$ENDIF}
+
+interface
+
+uses
+{$IFDEF FPC}
+  LResources,
+  LCLType,
+{$ENDIF}
+  Classes, Controls, Forms, StdCtrls, uJoyGlobals;
+
+type
+  TfrmJoyActions = class(TForm)
+    grpChangeActions: TGroupBox;
+    btnOK: TButton;
+    btnCancel: TButton;
+    radCADefault: TRadioButton;
+    radCAScript: TRadioButton;
+    radCAMessage: TRadioButton;
+    lblMailbox: TLabel;
+    cboMailbox: TComboBox;
+    procedure ActionsClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    function GetCA: TMotorChangedAction;
+    function GetInbox: integer;
+    procedure SetCA(const Value: TMotorChangedAction);
+    procedure SetInbox(const Value: integer);
+    { Private declarations }
+  public
+    { Public declarations }
+    property ChangeAction : TMotorChangedAction read GetCA write SetCA;
+    property Inbox : integer read GetInbox write SetInbox;
+  end;
+
+var
+  frmJoyActions: TfrmJoyActions;
+
+implementation
+
+{$IFNDEF FPC}
+{$R *.dfm}
+{$ENDIF}
+
+{ TfrmJoyActions }
+
+function TfrmJoyActions.GetCA: TMotorChangedAction;
+begin
+  if radCADefault.Checked then
+    Result := mcaDefault
+  else if radCAScript.Checked then
+    Result := mcaScript
+  else
+    Result := mcaMessage;
+end;
+
+function TfrmJoyActions.GetInbox: integer;
+begin
+  Result := cboMailbox.ItemIndex;
+end;
+
+procedure TfrmJoyActions.SetCA(const Value: TMotorChangedAction);
+begin
+  case Value of
+    mcaDefault : radCADefault.Checked := True;
+    mcaScript  : radCAScript.Checked  := True;
+  else
+    radCAMessage.Checked := True;
+  end;
+end;
+
+procedure TfrmJoyActions.SetInbox(const Value: integer);
+begin
+  cboMailbox.ItemIndex := Value;
+end;
+
+procedure TfrmJoyActions.ActionsClick(Sender: TObject);
+begin
+  cboMailbox.Enabled := radCAMessage.Checked;
+end;
+
+procedure TfrmJoyActions.FormCreate(Sender: TObject);
+begin
+  ActionsClick(Sender);
+end;
+
+{$IFDEF FPC}
+initialization
+  {$i uJoyActions.lrs}
+{$ENDIF}
+
+end.
